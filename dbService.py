25d634f95323f2700f5b3c9068b3f7288be4666b
Jaffer <sjafar@approcessing.com>
2022-03-08 11:28:00 +0500

    changes

--- dbService.py
+++ dbService.py
@@ -1,19 +1,35 @@
-import mysql.connector
+from server import mysql
+
+def addCartTransaction (cart_data) :
+    cur = mysql.connection.cursor()
+    
+    for i in cart_data:
+        item = i["name"]
+        price = i["quantity"]
+        quantity = i["price"]
+        userID = 1
+        query ="""insert into order_test2 (customer_id,item_name,Price) 
+            values ({0},'{1}',{2})""".format(userID,item,str(price))
+        cur.execute(query)
+    mysql.connection.commit()
+    cur.close()
 
 
-mydb=mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='12345',
-    database='db2'
-    )
-
-cur=mydb.cursor()
 
 
-s="create table order_test2( customer_id INT NOT NULL ,order_id INT NOT NULL AUTO_INCREMENT,item_name VARCHAR(100) NOT NULL, Price INT NOT NULL, PRIMARY KEY ( order_id ))"
+# mydb=mysql.connector.connect(
+#     host='localhost',
+#     user='root',
+#     password='12345',
+#     database='db2'
+#     )
 
-cur.execute(s)
+# cur=mydb.cursor()
+
+
+# s="create table order_test2( customer_id INT NOT NULL ,order_id INT NOT NULL AUTO_INCREMENT,item_name VARCHAR(100) NOT NULL, Price INT NOT NULL, PRIMARY KEY ( order_id ))"
+
+# cur.execute(s)
 
 # mydb=mysql.connector.connect(
 # host='localhost',
@@ -32,3 +48,4 @@ cur.execute(s)
 
 # cur.execute(s,o)
 # mydb.commit()
+
--- server.py
+++ server.py
@@ -1,16 +1,28 @@
+from gevent import config
 from pyttsx3 import speak
 import configs
 import requests
 import Prototype as pt
+import dbService as dbs
 from flask import Flask, request
 import json
 from flask_cors import CORS
 
 
 app = Flask(__name__)
+
+
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_DB'] = 'db2'
+ 
+mysql = MySQL(app)
 CORS(app)
-
-
 
 # Adds support for GET requests to our webhook
 @app.route('/webhook', methods=['GET'])
@@ -30,6 +42,14 @@ def index():
         msg = pt.extract(y['message'])
         print(msg)
         return msg, 200
+
+@app.route('/addTrans', methods=["GET", "POST"])
+def addTrans():
+    if request.method == 'POST':
+        #do something.....
+        y = json.loads(request.data)
+        dbs.addCartTransaction(y)
+        return 'record added', 200
 
 
 if __name__ == "__main__":
